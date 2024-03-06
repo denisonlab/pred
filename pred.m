99ac965035bc49f7b789269efaa2e97cc453f7fc
melall31 <melall@bu.edu>
2024-03-06 10:05:59 -0500

    fix 1

--- pred.m
+++ pred.m
@@ -64,7 +64,7 @@ if p.eyeTracking==1
         mkdir(data.eyeDataDir)
     end
     % Check to see if this eye file already exists
-    if exist(sprintf('%s/%s_s%d_eye.edf',data.eyeDataDir,p.subjectID,p.sessionNum),'file')
+    if exist(sprintf('%s/%s_s%d_eye.edf',data.eyeDataDir,p.subjectID,p.sessionNum),eyeFile)
         error('This subject/session already has saved data');
     end
 end
